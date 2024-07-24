1fc1565911cbad90b2aeaaabe49af9063e2e8f90
Deepak Pradeep <deepakpradeepbe@gmail.com>
2024-07-24 18:19:09 +0530

    Update my_s3_test.py

--- my_s3_test.py
+++ my_s3_test.py
@@ -1,22 +1,33 @@
-import os
-import base64
+def extract_timestamp(db_name):
+    match = re.search(r'dev_gk_output_step(\d{8})_test', db_name)
+    if match:
+        try:
+            # Check if the date is valid
+            return datetime.strptime(match.group(1), '%Y%m%d')
+        except ValueError:
+            # Invalid date
+            return None
+    return None
 
-def decode_file(enc_file_path, txt_file_path):
-    with open(enc_file_path, 'rb') as enc_file:
-        encoded_data = enc_file.read()
-    
-    decoded_data = base64.b64decode(encoded_data)
-    
-    with open(txt_file_path, 'wb') as txt_file:
-        txt_file.write(decoded_data)
+def cleanup_older_glue_database():
+    glue_client = session.client('glue')
+    list_database = glue_client.get_databases()
+    databases = list_database['DatabaseList']
 
-def convert_enc_to_txt_in_folder(folder_path):
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.enc'):
-            enc_file_path = os.path.join(folder_path, filename)
-            txt_file_path = os.path.splitext(enc_file_path)[0] + '.txt'
-            decode_file(enc_file_path, txt_file_path)
-            print(f'Converted {enc_file_path} to {txt_file_path}')
-
-folder_path = 'test_folders'
-convert_enc_to_txt_in_folder(folder_path)
+    # Filter databases by the pattern and extract timestamp
+    db_timestamps = [(db['Name'], extract_timestamp(db['Name'])) for db in databases]
+    # Remove entries with no timestamp or invalid timestamp
+    db_timestamps = [entry for entry in db_timestamps if entry[1] is not None]
+    # Sort databases by timestamp in descending order
+    db_timestamps.sort(key=lambda x: x[1], reverse=True)
+    # Keep the latest 3 databases
+    database_to_keep = db_timestamps[:3]
+    # Set of database names to keep
+    database_to_keep_set = {db[0] for db in database_to_keep}
+    # Prepare databases to delete
+    database_to_delete = [db_name for db_name, _ in db_timestamps if db_name not in database_to_keep_set]
+    logger.info(f'Deleting the older Glue databases based on Timestamp {database_to_delete}')
+    # Delete databases that are not in the keep_set
+    for db_name in database_to_delete:
+        logger.info(f'Deleting the Glue database {db_name}')
+        glue_client.delete_database(Name=db_name)
